13c945370581b31ef7c1c97172984c31dce54f5f
John Mendonca <33038989+mfsmm@users.noreply.github.com>
2018-04-12 20:24:11 +0100

    Clipboard interaction
    Added code regarding user/clipboard interaction, including copy and paste(both sides). NOT TESTED

--- clipboard.c
+++ clipboard.c
@@ -1,22 +1,47 @@
+#include "clipboard.h"
+
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <sys/socket.h>
 
+#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-#include "clipboard.h"
-
  
 int main(){
-	int clipboard_id;
+	int sockt_id;
 
-
-	if((clipboard_id = clipboard_connect("doesnt matter right now"))==-1){
+	if((sockt_id = clipboard_connect("doesnt matter right now"))==-1){
 		exit(-1);
 	}
 
+	//Creates message handlers(assuming right now that clipboard only deals with strings)
+	char** clipboard = (char*)malloc(10*sizeof(char*));
+	char* msg = (char*)malloc(sizeof(_msg));
+	_msg* send_msg = (_msg*)malloc(sizeof(_msg));
+
+	//Clipboard awaits orders
+	while(1){
+
+		if(recv(sockt_id, msg, sizeof(_msg),0) == -1){
+			perror("receive");
+		}
+		//de-codes message
+		memcpy(send_msg, msg);
+
+		if(send_msg->option == WRITE_REQUEST){
+			printf("received and writing to region %d\n",send_msg->region);
+			clipboard[send_msg->region] = send_msg->message;
+		}
+		//READ_REQUEST
+		else{
+			printf("sending message from region %d", send_msg->region);
+			if(send(sockt_id, clipboard[send_msg->region], sizeof(clipboard[send_msg->region],0) == -1)){
+				perror("send");
+			}
+		}
+	}
 	exit(0);
 	
 }
--- library.c
+++ library.c
@@ -1,11 +1,18 @@
 #include "clipboard.h"
+
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/socket.h>
+
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 
+/*This function should try to do a connect to a server whose socket is created in the
+clipboard_dir. This function returns the file descriptor of the newly created and connected socket on error
+*/
 int clipboard_connect(char * clipboard_dir){
 	struct sockaddr_un local_addr;		//Isto torna só valido para AF_UNIX
 
@@ -34,18 +41,63 @@ int clipboard_connect(char * clipboard_dir){
 	}
 	return sock_fd;
 
-	/*Agora devemos lançar um processo que esteja constantemente a ouvir pedidos para o socket
-	e guardar a info algures.
-	 Depois o clip_board copy/paste só acedem a essa info guardadam e tratam dela right?
-	 */
-
 }
 
+/* This function receives the value returned by clipboard_connect and uses it to send/copy
+data to clipboard, using a socket (all reads and writes will be done in the same file
+descriptor)
+*/
 int clipboard_copy(int clipboard_id, int region, void *buf, size_t count){
 
+	//allocates memory for structures
+	char* msg = (char*)malloc(sizeof(_msg));
+	_msg* send_msg = (_msg*)malloc(sizeof(_msg));
+	
+	//fills structure for transit
+	send_msg->option = WRITE_REQUEST;
+	send_msg->region = region;
+	send_msg->message = buf;
+	send_msg->msg_size = count;
+	
+	memcpy(msg,send_msg, sizeof(_msg));
+	
+	//sends message to clipboard
+	if (send(clipboard_id,msg,sizeof(_msg),0) == -1){
+		perror("send");
+	}
+
+	//frees temp message
+	free(send_msg);
 	
 }
+
+
+/*This function receives the value returned by clipboard_connect and uses it to
+retrieve/paste data from the clipboard, using a socket (all reads and writes will be done in
+the same file descriptor)
+*/
 int clipboard_paste(int clipboard_id, int region, void *buf, size_t count){
 
+	//allocates memory for structures
+	char* msg = (char*)malloc(sizeof(_msg));
+	_msg* send_msg = (_msg*)malloc(sizeof(_msg));
 	
+	//fills structure for transit
+	send_msg->option = READ_REQUEST;
+	send_msg->region = region;
+	
+	memcpy(msg,send_msg, sizeof(_msg));
+
+	
+	//writes request to clipboard	
+	if(send(clipboard_id,msg,sizeof(_msg)) == -1){
+		perror("send");
+	}
+	
+	//reads buf message from clipbord
+	if(recv(clipboard_id, buf, sizeof(count), 0) == -1 ){
+		perror("receive");
+	}
+
+	free(send_msg);
 }
